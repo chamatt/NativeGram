58006068eca0c5002d35049c1d0750840d3eba3b
chamatt <matt.vicent@gmail.com>
2020-04-03 05:19:55 -0300

    Add Keyboard Aware Scroll View to SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Dimensions } from "react-native";
 import { Layout, Text, Input, Icon, Button } from "@ui-kitten/components";
 import { Container } from "./styles";
 import SizedBox from "~/components/SizedBox";
 import { useStoreActions, useStoreState } from "easy-peasy";
 import LoadingIndicator from "~/components/LoadingIndicator";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-const StarIcon = style => <Icon {...style} name="star" />;
+const HEIGHT = Dimensions.get("screen").height;
 
 export default function SignUp({ navigation }) {
   const [username, setUsername] = useState();
@@ -18,46 +19,49 @@ export default function SignUp({ navigation }) {
 
   return (
     <Container level="2">
-      <SafeAreaView>
-        <Input
-          autoCapitalize="none"
-          label="Username"
-          placeholder="johndoe"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <SizedBox height={20} />
-        <Input
-          label="Email"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          placeholder="john.doe@example.com"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <SizedBox height={20} />
-        <Input
-          label="Password"
-          secureTextEntry
-          placeholder="**************"
-          value={password}
-          onChangeText={setPassword}
-        />
-        <SizedBox height={40} />
-        <Button
-          icon={
-            loading
-              ? style => <LoadingIndicator {...style}></LoadingIndicator>
-              : null
-          }
-          disabled={loading}
-          appearance="filled"
-          status="primary"
-          onPress={() => signUp({ username, email, password })}
-        >
-          {!loading && "CREATE ACCOUNT"}
-        </Button>
-      </SafeAreaView>
+      <KeyboardAwareScrollView>
+        <SizedBox height={HEIGHT / 6} />
+        <SafeAreaView>
+          <Input
+            autoCapitalize="none"
+            label="Username"
+            placeholder="johndoe"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <SizedBox height={20} />
+          <Input
+            label="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder="john.doe@example.com"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <SizedBox height={20} />
+          <Input
+            label="Password"
+            secureTextEntry
+            placeholder="**************"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SizedBox height={40} />
+          <Button
+            icon={
+              loading
+                ? style => <LoadingIndicator {...style}></LoadingIndicator>
+                : null
+            }
+            disabled={loading}
+            appearance="filled"
+            status="primary"
+            onPress={() => signUp({ username, email, password })}
+          >
+            {!loading && "CREATE ACCOUNT"}
+          </Button>
+        </SafeAreaView>
+      </KeyboardAwareScrollView>
     </Container>
   );
 }
